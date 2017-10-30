6ef5401a2bc6739ed1cf35c000b7ea6c4956a5d5
Humair Khan <ehumairkhan@gmail.com>
2017-10-30 12:34:17 -0400

    Removed line example as its not needed

--- app/javascript/main_page/main_page.jsx
+++ app/javascript/main_page/main_page.jsx
@@ -5,13 +5,8 @@ import ChartComponent from '../pf-lib/c3Charts/chart.jsx';
 
 export class main_page extends Component {
   render() {
-
-    // Build config for line chart
-    let defaultConfig = $().c3ChartDefaults().getDefaultSparklineConfig();
-    let lineConfig =  {...defaultConfig, ...CHART_CONFIG.line};
-
     // Build config for donut chart
-    defaultConfig = $().c3ChartDefaults().getDefaultDonutConfig();
+    let defaultConfig = $().c3ChartDefaults().getDefaultDonutConfig();
     let donutConfig =  {...defaultConfig, ...CHART_CONFIG.donut};
 
     // This call back will be invoked once the donut chart is generated
@@ -33,20 +28,11 @@ export class main_page extends Component {
               </h2>
               <div className="card-pf-footer row">
                 <div className="col-sm-offset-4 col-sm-4 ">
-                  <h4> Line Chart Example: </h4>
-                  {/* Chart component takes the element and config as a prop */}
-                  <ChartComponent element='chart-pf-sparkline' config={lineConfig}/>
-                </div>
-              </div>
-
-              <div className="card-pf-footer row">
-                <div className="col-sm-offset-4 col-sm-4 ">
                   <h4> Donut Chart Example: </h4>
                   {/* Chart component can also take a callback that is invoked once the chart is generated */}
                   <ChartComponent element='chart-pf-donut' config={donutConfig} generateCallback={generateCallback}/>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
